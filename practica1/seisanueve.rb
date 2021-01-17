22852f2e9e587ef8321a8e5edf44ad1f74035ba3
gonnicobilbao@gmail.com <gonnicobilbao@gmail.com>
2021-01-17 20:56:53 -0300

    practica siesnueve

--- practica1/seisanueve.rb
+++ practica1/seisanueve.rb
@@ -1,4 +1,10 @@
 #puntos 6 a 9
+#6.- Escribir la función “filtrar_millennials” que tome de parámetro un arreglo de años [1999, 2001, 2005, … ], 
+# y devuelva un arreglo filtrando los años que son mayores a 1980 y menores a 1995.
+#7.- Escribir una función que tome como parámetro un arreglo con nombres, y que imprima cada nombre de ese arreglo al revés. *Ver método “reverse” 
+#8.- Escribir una función que dado un arreglo de sueldos, calcule y retorne la suma total
+#9.- Escribir una función que dado un arreglo de sueldos, devuelva un arreglo con un aumento de un 28% en cada sueldo.
+
 
 def filtrar_millenials(arreglo)
     arreglo.select { |n|  (1980..1995) === n }
